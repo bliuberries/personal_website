bcad6f52cca17c1812a7e23251720dade0ae5c1b
Brady <brady.y.liu@gmail.com>
2020-01-29 23:37:09 -0800

    Progress in the making..

--- src/App.js
+++ src/App.js
@@ -1,26 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import Icon from './icon/icon.jsx';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+export default class App extends Component {
 
-export default App;
+
+  render() {
+    const navStyle = {
+      textDecoration: "none",
+      color: "#fff",
+      cursor: "pointer",
+    }
+    return (
+      <div className="App">
+        <div className="Home-page">
+          <div className="logo">
+            <div></div>
+          </div>
+          <div>
+            <Icon />
+            <div>
+              <a className="navigation-title" href="/contact" style={navStyle} >
+                Contact
+              </a>
+            </div>
+            <div>
+              <a className="navigation-title" href="/about" style={navStyle} >
+                About
+              </a>
+            </div>
+            <div>
+              <a className="navigation-title" href="/resume" style={navStyle} >
+                Resume
+              </a>
+            </div>
+            <div>
+              <a className="navigation-title" href="/portfolio" style={navStyle} >
+                Portfolio
+              </a>
+            </div>
+            <div>
+              <a className="navigation-title" href="/" style={navStyle} >
+                Home
+              </a>
+            </div>
+          </div>
+          <header className="App-header">
+            <span className="full-name">DEVELOPER.</span>
+          </header>
+        </div>
+      </div>
+    )
+  }
+} 
