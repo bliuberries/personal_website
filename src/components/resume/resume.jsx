d5fc6d8673b610ec5432beb307c19b4126f5de2f
Brady <brady.y.liu@gmail.com>
2020-01-31 18:57:43 -0800

    Added resume

---
+++ src/components/resume/resume.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Header from './../header/header.jsx';
+
+const Resume = () => {
+  return (
+    <div className='resume'>
+      <Header />
+      <div className='resume-container'>
+      <a href="" target="blank"></a>
+      </div>
+    </div>
+  )
+}
+
+export default Resume;
