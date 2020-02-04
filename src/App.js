2ff2d5cf32c882b96c7966febe7c44719be6a30b
Brady <brady.y.liu@gmail.com>
2020-02-03 18:01:50 -0800

    Modifying portfolio

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Home from './components/home/home.jsx';
 import About from './components/about/about.jsx';
 // import Resume from './components/resume/resume.jsx';
 // import Contact from './components/contact/contact.jsx';
-// import Portfolio from './components/portfolio/portfolio.jsx';
+import Portfolio from './components/portfolio/portfolio.jsx';
 
 import './App.css';
 
@@ -16,7 +16,7 @@ const App = () => {
       <Switch>
         <Route exact path='/' render={() => <Home />} />
         <Route exact path='/about' component={About} />
-        {/* <Route exact path='/portfolio' component={Portfolio} /> */}
+        <Route exact path='/portfolio' component={Portfolio} />
         {/* <Route exact path='/resume' component={Resume} /> */}
         {/* <Route exact path='/contact' component={Contact} /> */}
       </Switch>
