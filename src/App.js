f035b263ebe620fb0d88b03e9fe48db9c16543d1
Brady <brady.y.liu@gmail.com>
2020-02-03 21:54:54 -0800

    Added Contact and resume

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import { Switch, Route } from "react-router-dom";
 
 import Home from './components/home/home.jsx';
 import About from './components/about/about.jsx';
-// import Resume from './components/resume/resume.jsx';
-// import Contact from './components/contact/contact.jsx';
+import Resume from './components/resume/resume.jsx';
+import Contact from './components/contact/contact.jsx';
 import Portfolio from './components/portfolio/portfolio.jsx';
 
 import './App.css';
@@ -17,8 +17,8 @@ const App = () => {
         <Route exact path='/' render={() => <Home />} />
         <Route exact path='/about' component={About} />
         <Route exact path='/portfolio' component={Portfolio} />
-        {/* <Route exact path='/resume' component={Resume} /> */}
-        {/* <Route exact path='/contact' component={Contact} /> */}
+        <Route exact path='/resume' component={Resume} />
+        <Route exact path='/contact' component={Contact} />
       </Switch>
     </div>
   )
