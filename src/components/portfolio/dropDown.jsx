49db85d3532d680aa0bf14ec872c9825a9b0e995
Brady <brady.y.liu@gmail.com>
2020-02-03 21:53:44 -0800

    Destructored props

--- src/components/portfolio/dropDown.jsx
+++ src/components/portfolio/dropDown.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 
 const Dropdown = (props) => {
+  const { fn } = props;
+
   return (
     <div className='project-list-container'>
       <div className='project-list-title'>
         PROJECTS:
-          <select className='project-name' onChange={props.props}>
+          <select className='project-name' onChange={fn}>
           <option>Super Mario Clone</option>
           <option>To Do App</option>
           <option>Tetris</option>
