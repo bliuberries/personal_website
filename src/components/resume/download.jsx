433084e0a8e2adb92dafcdafe729dd074507b6e2
Brady <brady.y.liu@gmail.com>
2020-02-05 17:13:08 -0800

    Changed to scss

--- src/components/resume/download.jsx
+++ src/components/resume/download.jsx
@@ -4,7 +4,7 @@ import resumeStyles from './resume.module.scss';
 const Download = () => {
   return (
       <a 
-      className={resumeStyles.downloadButton}
+      className={resumeStyles.button}
       href="https://github.com/bliuberries/resume/raw/master/Brady-Liu-%20Resume.pdf" target="_blank"
       rel="noopener noreferrer"
       >Download Resume →</a>
