99a4dc43eda066a494643fedd74cfc84b6ac94c8
Brady <brady.y.liu@gmail.com>
2020-02-05 17:10:29 -0800

    Decoupled code

---
+++ src/components/header/pages.js
@@ -0,0 +1,24 @@
+const pages = [
+  {
+    title: 'Contact',
+    alias: '/contact',
+  },
+  {
+    title: 'Resume',
+    alias: '/resume',
+  },
+  {
+    title: 'Portfolio',
+    alias: '/portfolio',
+  },
+  {
+    title: 'About Me',
+    alias: '/about',
+  },
+  {
+    title: 'Home',
+    alias: '/',
+  },
+];
+
+export default pages;
