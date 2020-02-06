4bafca5ce13a5f0a98839dc6004ed81748b36d9d
Brady <brady.y.liu@gmail.com>
2020-02-05 17:11:56 -0800

    Decoupled code

--- src/components/contact/contact.jsx
+++ src/components/contact/contact.jsx
@@ -1,19 +1,22 @@
 import React from 'react';
+import ContactModal from './contactModal.jsx';
 
 const Contact = () => {
-
   return (
     <div className='contact'>
       <div className="background" >
         <div className="center-text-container">
           <strong className="center-text">Connect with</strong>
-          <br/>
+          <br />
           <strong className="center-text">me today.</strong>
         </div>
       </div>
       <div className='resume-container'>
-      
-      </div>
+        content
+      <br />
+        more content
+      <ContactModal />
+        </div>
     </div>
   )
 }
