9677fc5f04a6932a0738e4544f088fc92e51ef6c
Brady <brady.y.liu@gmail.com>
2020-02-09 16:13:49 -0800

    Changed colors

--- src/components/contact/contact.jsx
+++ src/components/contact/contact.jsx
@@ -2,25 +2,28 @@ import React from 'react';
 import ContactModal from './contactModal.jsx';
 import contactStyles from './contact.module.scss';
 
-const Contact = () => {
-  return (
-    <div className='contact'>
-      <div className="background" >
-        <div className="center-text-container">
-          <strong className="center-text">Connect with</strong>
-          <br />
-          <strong className="center-text">me today.</strong>
-        </div>
-      </div>
-      <div className={contactStyles.contactContainer}>
-        <div>
-          this will be an image
-        </div>
-        <ContactModal />
-      </div>
-    </div>
-  )
-}
+import veggie from './../../images/oneVeggie.png';
+// const Contact = () => {
 
+class Contact extends React.Component {
+
+  render() {
+    return (
+      <div className='contact'>
+        <div className="background" >
+          <div className="center-text-container">
+            <strong className="center-text">Connect with</strong>
+            <br />
+            <strong className="center-text">me today.</strong>
+          </div>
+        </div>
+        <div className={contactStyles.contactContainer}>
+          <img className={contactStyles.image} src={veggie} alt=""/>
+          <ContactModal />
+        </div>
+      </div>
+    )
+  }
+}
 export default Contact;
 
