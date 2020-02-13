f260aa182669e3aec42b583e6770c2ae817ec2e3
Brady <brady.y.liu@gmail.com>
2020-02-12 18:51:43 -0800

    Deleted images and hosted on imgur

---
+++ src/components/images.js
@@ -0,0 +1,11 @@
+const images = {
+  about: 'https://imgur.com/sSsuVL0.png',
+  aboutSnip: 'https://i.imgur.com/kBs1NnH.png',
+  contact: 'https://imgur.com/Vwr6HJg.png',
+  contactSnip: 'https://imgur.com/UBcwRCM.png',
+  home: 'https://imgur.com/VfabQxi.png',
+  portfolio: 'https://imgur.com/WWhQH33.png',
+  resume: 'https://imgur.com/elDesEk.png'
+}
+
+export default images;
