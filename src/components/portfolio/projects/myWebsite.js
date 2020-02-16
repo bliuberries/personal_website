1bbda76605685a5d98c83122f790ab0c0ffdefa2
Brady <brady.y.liu@gmail.com>
2020-02-15 23:10:59 -0800

    Changed tech stack

--- src/components/portfolio/projects/myWebsite.js
+++ src/components/portfolio/projects/myWebsite.js
@@ -7,7 +7,7 @@ const myWebsite = {
   url: 'https://www.bradyliu.com/',
   github: 'https://github.com/bliuberries/personal_website',
   description: 'Creating this from scratch really tested my abilities as a developer. I would constantly build something out, and then delete it just to rebuild it again differently. The way I import images and handle how I manipulate files so that I can go straight to an individual piece that I want to change with ease. My favorite and most hated thing about this project was being able to change the color of the header based on what page I was on. I probably re-wrote that piece of code five times, changing small bits between multiple files before ultimately being satisfied with how I decoupled it. What I learned about this is that perfection is not a destination but a journey and a never ending one at that.',
-  techStack: 'React, Javascript, Netlify'
+  techStack: 'React, Javascript, Netlify, Sass'
 }
 
 export default myWebsite;
