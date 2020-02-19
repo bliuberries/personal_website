30ae7d1f2ea4e5acbda936d5db9c73c8b3366ad1
Brady <brady.y.liu@gmail.com>
2020-02-18 22:46:24 -0800

    Created new menu for both mobile and desktop

---
+++ src/components/header/hamburgerMenu/navbar.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import Icon from '../icon/icon.jsx';
+
+import Hamburger from './sidePanel/hamburgerMenu.jsx';
+import hStyles from '../header.module.scss';
+
+const Navbar = props => (
+  <header className={hStyles.toolBar}>
+    <nav className={hStyles.toolBarNav}>
+      <div className={hStyles.hamburgerMenu}>
+        <Hamburger click={props.openClose}/>
+      </div>
+      <div className={hStyles.toolBarLogo}>
+        <Icon />
+      </div>
+      <div className={hStyles.spacer}/>
+      <div className={hStyles.navItems}>
+        <ul className={hStyles.toolbarNavUl}> 
+          <Link to='/' className={hStyles.navItem}>Home</Link>
+          <Link to='/about'className={hStyles.navItem}>About</Link>
+          <Link to='/portfolio'className={hStyles.navItem}>Portfolio</Link>
+          <Link to='/resume'className={hStyles.navItem}>Resume</Link>
+          <Link to='/contact'className={hStyles.navItem}>Contact</Link>
+        </ul>
+      </div>
+    </nav>
+  </header>
+)
+
+export default Navbar
