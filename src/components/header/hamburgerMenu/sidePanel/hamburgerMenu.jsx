e3d0908ddb1b20ccd114b2c3415e81f9ae5611a1
Brady <brady.y.liu@gmail.com>
2020-02-18 23:37:01 -0800

    Refactored code

--- src/components/header/hamburgerMenu/sidePanel/hamburgerMenu.jsx
+++ src/components/header/hamburgerMenu/sidePanel/hamburgerMenu.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import hStyles from '../../header.module.scss';
 
 
-const Hamburger = props => (
+const Hamburger = props => {
+  return (
   <button className={hStyles.hamburgerContainer} onClick={props.click}>
     <div className={hStyles.line}/>
     <div className={hStyles.line}/>
     <div className={hStyles.line}/>
   </button>
-)
+  );
+}
 
 export default Hamburger;
