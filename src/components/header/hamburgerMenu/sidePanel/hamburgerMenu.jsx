21fdb024992c703d1148464db1d88cfd8e7e25cd
Brady <brady.y.liu@gmail.com>
2020-02-18 22:46:46 -0800

    Added hamburger menu icon

---
+++ src/components/header/hamburgerMenu/sidePanel/hamburgerMenu.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import hStyles from '../../header.module.scss';
+
+
+const Hamburger = props => (
+  <button className={hStyles.hamburgerContainer} onClick={props.click}>
+    <div className={hStyles.line}/>
+    <div className={hStyles.line}/>
+    <div className={hStyles.line}/>
+  </button>
+)
+
+export default Hamburger;
