dacf9add5e9916e574d61ae575a54577f80ed825
Brady <brady.y.liu@gmail.com>
2020-02-18 22:47:58 -0800

    Replaced menu component

--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -1,29 +1,70 @@
 import React from 'react';
-import Header from '../header/header.jsx';
+
+// import Header from '../header/header.jsx';
+
+import Navbar from '../header/hamburgerMenu/navbar.js';
+
 import CenterText from './centerText.jsx';
 import homeStyles from './home.module.scss';
+import SidePanel from '../header/hamburgerMenu/sidePanel/sidePanel.jsx';
+import Backdrop from '../header/hamburgerMenu/backdrop.js';
 
-const Home = (props) => {
-  let { cText, Button, location} = props;
-  if(location === undefined) location = 'Home'
 
-  return (
-    <div>
-      <Header location={location}/>
-      <div className="background" >
-        <div className={homeStyles.centerTextContainer}>
-          {cText.map((text, index) => {
-            return (
-              <CenterText text={text} key={index}/>
-            )
-          })}
-          <br/>
-          {Button !== undefined ? <Button className={homeStyles.button}/> : null}
+class Home extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      cText: ['Developer'],
+      Button: undefined,
+      location: 'Home',
+      sidePanelOpen: false,
+    }
+  }
+
+  componentDidMount() {
+    this.setState((prevState) => {
+      return {
+        cText: this.props.cText,
+        Button: this.props.Button,
+        locat: this.props.location
+      }
+    })
+  }
+
+  openCloseSidePanel = () => {
+    this.setState((prevState) => {
+      return { sidePanelOpen: !prevState.sidePanelOpen }
+    })
+  }
+
+  backdropClickHandler = () => {
+    this.setState({sidePanelOpen: false})
+  }
+
+  render() {
+    let backdrop;
+    if(this.state.sidePanelOpen) {
+      backdrop = <Backdrop click={this.backdropClickHandler}/>
+    }
+    return (
+      <div>
+        <Navbar openClose={this.openCloseSidePanel}/>
+        <SidePanel show={this.state.sidePanelOpen}/>
+        {backdrop}
+        <div className="background" >
+          <div className={homeStyles.centerTextContainer}>
+            {this.state.cText.map((text, index) => {
+              return (
+                <CenterText text={text} key={index} />
+              )
+            })}
+            <br />
+            {this.state.Button !== undefined ? <this.state.Button className={homeStyles.button} /> : null}
+          </div>
         </div>
       </div>
-    </div>
-  )
+    )
+  }
 }
-
 
 export default Home;
