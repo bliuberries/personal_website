12ad921a5ac28cb53b28b7fc1cbec65ec35f64cd
Brady <brady.y.liu@gmail.com>
2020-02-20 15:29:41 -0800

    Modified tetris component

--- src/components/portfolio/projects/tetris.js
+++ src/components/portfolio/projects/tetris.js
@@ -1,14 +1,13 @@
 const tetris = {
   images: [
-    {src: 'https://i.imgur.com/WsfLYCF.gif'},
-    {src: 'https://i.imgur.com/v4NvXIL.gif'},
-    {src: 'https://i.imgur.com/vt7BJwm.gif'},
-    {src: 'https://i.imgur.com/tAerlpv.gif'}
+    {src: 'https://imgur.com/REtMec3.gif'},
+    {src: 'https://imgur.com/DWLWLgo.gif'},
+    {src: 'https://imgur.com/StVQ7Yw.gif'}
   ],
   title: 'Tetris',
-  url: 'https://github.com/bliuberries/tetris',
+  url: 'https://bliuberries.github.io/tetris/',
   github: 'https://github.com/bliuberries/tetris',
-  description: 'I made this',
+  description: 'Tetris was a fun project where I decided to first test my abilities as a developer. From introducing the notion of "gravity" to blocks being stackable, to making them disappear was a painfully fun process. Overall, I learned a lot about DOM manipulation through this project and I plan on expanding my horizon for future projects to test new areas of expertise.',
   techStack: 'Javascript'
 }
 
