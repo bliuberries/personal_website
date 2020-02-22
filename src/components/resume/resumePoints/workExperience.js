42b138a44e3efd08c512e01f68bb4505e7119b72
Brady <brady.y.liu@gmail.com>
2020-02-21 19:45:10 -0800

    Updated resume

--- src/components/resume/resumePoints/workExperience.js
+++ src/components/resume/resumePoints/workExperience.js
@@ -6,7 +6,18 @@ const workExperience = [
         name: 'Craig Technologies',
         place: 'Vandenberg, CA',
         date: 'Jan 2019 - June 2019'
-      },
+      }
+    ],
+    experience: [
+      'Loaded new machines with updated software to match security policies',
+      'Instructed users on best practices to mitigate small but time-consuming issues',
+      'worked with third party vendors to handle warranty and misused equipment'
+    ],
+    key: []
+  },
+  {
+    role: 'CLIENT SYSTEMS TECHNICIAN',
+    companies: [
       {
         name: 'U.S. Air Force',
         place: 'Overseas',
@@ -17,28 +28,15 @@ const workExperience = [
       'Troubleshoot with clients identifying computer hardware/software probems on classified systems',
       'Created SOPs for newly identified issues with dynamic solutions for teams and clients',
       'Coached new IT personnel on advanced technical problem solving and best security practices',
-      'Key player as part of emergency task force in high-stress environments'
+      'Key player as part of emergency task force in high-stress environments',
+      'Managed 211 accounts of IT equipment valued at $24m, training all new equipment custodians',
+      'Conducted and pipelined Staff Assisted Visits for QA resulting in a 70% increase in compliance',
+      'Sanitized, documented and disposed of 416 hard drives consistening of sensitive information'
     ],
     key: [
       'Completed 23% of all trouble tickets for the past year in a team of 8. (USAF)',
       'Remedied long-standing issue affecting users worldwide. (USAF)'
     ]
-  },
-  {
-    role: 'BASE EQUIPMENT CUSTODIAN OFFICER',
-    companies: [
-      {
-        name: 'U.S. Air Force',
-        place: 'Overseas',
-        date: 'Apr 2015 - Apr 2016'
-      }
-    ],
-    experience: [
-      'Managed 211 accounts of IT equipment valued at $24m, training all new equipment custodians',
-      'Conducted and pipelined Staff Assisted Visits for QA resulting in a 70% increase in compliance',
-      'Sanitized, documented and disposed of 416 hard drives consistening of sensitive information'
-    ],
-    key: []
   }
 ]
 
