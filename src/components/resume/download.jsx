3a12cabb94a28c13fb581527382e61164e5850c8
Brady <brady.y.liu@gmail.com>
2020-03-05 13:02:16 -0800

    Updated Resume

--- src/components/resume/download.jsx
+++ src/components/resume/download.jsx
@@ -5,7 +5,7 @@ const Download = () => {
   return (
       <a 
       className={resumeStyles.button}
-      href="https://github.com/bliuberries/resume/raw/master/Brady%20Liu%20Resume.pdf" target="_blank"
+      href="https://github.com/bliuberries/resume/raw/master/Brady%20Liu%20-%20Resume.pdf" target="_blank"
       rel="noopener noreferrer"
       >Download Resume → </a>
   )
