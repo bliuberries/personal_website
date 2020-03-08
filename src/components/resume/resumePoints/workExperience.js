1a9e434c0c287cb5395d5a5dc0fda76e24f484e7
Brady <brady.y.liu@gmail.com>
2020-03-07 16:34:06 -0800

    Fixed typos

--- src/components/resume/resumePoints/workExperience.js
+++ src/components/resume/resumePoints/workExperience.js
@@ -25,13 +25,13 @@ const workExperience = [
       }
     ],
     experience: [
-      'Troubleshoot with clients identifying computer hardware/software probems on classified systems',
+      'Troubleshoot with clients identifying computer hardware/software problems on classified systems',
       'Created SOPs for newly identified issues with dynamic solutions for teams and clients',
       'Coached new IT personnel on advanced technical problem solving and best security practices',
       'Key player as part of emergency task force in high-stress environments',
       'Managed 211 accounts of IT equipment valued at $24m, training all new equipment custodians',
       'Conducted and pipelined Staff Assisted Visits for QA resulting in a 70% increase in compliance',
-      'Sanitized, documented and disposed of 416 hard drives consistening of sensitive information'
+      'Sanitized, documented and disposed of 416 hard drives consisting of sensitive information'
     ],
     key: [
       'Completed 23% of all trouble tickets for the past year in a team of 8. (USAF)',
